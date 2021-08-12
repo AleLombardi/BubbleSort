fff2b1d5c4646fe2858f9e59708f5c9a54564f23
Ale <alessandro3lombardi@gmail.com>
2021-08-12 23:46:31 +0200

    mod

--- src/funzione/Funzione.java
+++ src/funzione/Funzione.java
@@ -16,12 +16,12 @@ public static void main(String []args){
 			
 			for(y=x;y<array.length -1;y++) {
 				
-				if(array[x]>array[y+1]) { 	//Comparazione se x è maggiore di y.
-					tmp = array[y+1]; 		//Quando x è maggiore, y viene messo nella variabile tmp(temporanea).
-					array[y+1]= array[x]; 	//Scambio di posizione x -> y.
-					array[x] = tmp;  		//x(4) posizione 0 passerà in posizione 1 scambiando di posto con y(1).
-											//Il ciclo si ripeterà finche troverà un numero maggiore del precedente.
-					}						//Il BubbleSort porta da sinistra a destra il numero più grande.
+				if(array[x]>array[y+1]) { 	
+					tmp = array[y+1]; 		
+					array[y+1]= array[x]; 	
+					array[x] = tmp;  		
+											
+					}						
 				System.out.println(Arrays.toString(array));
 			}
 			
